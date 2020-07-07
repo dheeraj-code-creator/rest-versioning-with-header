827ec60da22340d7f13f00c1c7db34323458e95e
unknown <dheeraj1990kk@gmail.com>
2020-07-07 19:32:07 +0530

    latest-version

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # all-in-one-java8-features
 
-http://localhost:9091/user-management/swagger-ui.html
+http://localhost:9091/user-management/swagger-ui/index.html?configUrl=%2Fuser-management%2Fv3%2Fapi-docs%2Fswagger-config
--- src/main/java/com/springboot/rest/controller/v1/UserController.java
+++ src/main/java/com/springboot/rest/controller/v1/UserController.java
@@ -3,7 +3,6 @@ package com.springboot.rest.controller.v1;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +11,15 @@ import com.springboot.rest.dto.UserDto;
 import com.springboot.rest.service.UserService;
 
 @RestController
-@RequestMapping(value = "v1/userinfo")
+@RequestMapping(value = "/userinfo", produces ="application/json")
 public class UserController {
+	
+	public static final String X_ACCEPT_VERSION_V1 = "X-Accept-Version" + "=" + "v1";
 
 	@Autowired
 	private UserService userService;
 
-	@GetMapping(value = "/alluser", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/alluser", headers = X_ACCEPT_VERSION_V1)
 	public List<UserDto> getUserinfo() {
 		List<UserDto> finalResults = userService.getAllUserInfo();
 		return finalResults;
